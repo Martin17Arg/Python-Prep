6847b603aefaffaf2c58df12da457b17c8f2c9e5
Martin <mtosi17@gmail.com>
2022-02-21 14:46:20 -0300

    initial commit

---
+++ 09 - Entrada-Salida y Manejo de Archivos/mcm.py
@@ -0,0 +1,21 @@
+def mcm(n):
+    """Mínimo común múltiplo del número 'n'.
+    """
+    assert (type(n) == int) and (n > 1), f'El número debe ser un entero positivo mayor a 1'
+        
+    divisores = range(2,n+1)
+    factores = dict()
+    
+    for div in divisores:
+        
+        while n % div == 0:
+            if div not in factores:
+                factores[div] = 1
+            else:
+                factores[div] += 1
+            n = n/div
+
+    lista_de_factores = [key for key in factores.keys()]
+    lista_de_exponentes = [exp for exp in factores.values()]
+
+    return lista_de_factores, lista_de_exponentes
